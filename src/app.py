5efed5bbeb84cf510562015fde489353d735c1ee
Juan Candia <juan.candia@swissmedical.com.ar>
2026-01-05 16:35:15 -0300

    add config

--- src/app.py
+++ src/app.py
@@ -5,6 +5,9 @@ import socket
 
 app = Flask(__name__)
 
+@app.route("/")
+def root():
+    return "OK", 200
 
 @app.route('/api/v1/info')
 
@@ -24,5 +27,5 @@ def health():
 
 if __name__ == '__main__':
 
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
 
