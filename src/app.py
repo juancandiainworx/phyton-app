6af0b6ef17ec9ad96237948ed0e771d989811fa4
jucandia <juan.candia@gmail.com>
2026-01-06 23:18:11 +0000

    add cambios

--- src/app.py
+++ src/app.py
@@ -31,7 +31,7 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'ESTE ES UN NUEVO MENSAJE DE DETALLES PARA VERIFICAR EL CI',
+        'message': 'ESTE ES UN NUEVO MENSAJE DE DETALLES',
     })
 
 if __name__ == '__main__':
