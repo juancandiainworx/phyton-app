7ccd3331d62b80150805589242ce5086285e32ce
jucandia <juan.candia@gmail.com>
2026-01-14 18:49:59 +0000

    Fix duplicated Flask endpoint info

--- src/app.py
+++ src/app.py
@@ -27,7 +27,7 @@ def health():
 
 @app.route('/api/v1/details')
 
-def info():
+def details():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
