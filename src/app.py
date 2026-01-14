8e7fc4783b92992c120d72589be4426bf671c4c7
jucandia <juan.candia@gmail.com>
2026-01-14 18:36:46 +0000

    add cambios

--- src/app.py
+++ src/app.py
@@ -15,7 +15,7 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'You are doing great, little human, Vddddd  rrr  yhghhhhfew <3',
+        'message': 'You are doing great, little human, Vdddhhhhdd  rrr  yhghhhhfew <3',
         'deployed_on': 'kubernetes'
     })
 
